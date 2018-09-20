7fc8d245019eb2e4d9d5b2dac9409530fc7b6306
Joseph Harwood <josephblakeharwood@gmail.com>
2018-09-20 20:51:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,16 +12,6 @@ class Dog
     @name = new_name
   end
   
-    def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
- 
-  def breed=(new_breed)
-    @breed = new_breed
-  end
+    
  
 end
